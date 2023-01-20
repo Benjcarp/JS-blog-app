5cce514c5ac6f427cbaf7c3f3bc350a67d1ff2e5
Benjamin Carpentier <benjamincarpentier01@gmail.com>
2023-01-20 12:04:07 +0100

    feat: supression d'articles

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -604,21 +604,43 @@ const displayArticles = articles => {
         <p class="article-author">${article.author}</p>
         <p class="article-content">${article.content}</p>
         <div class="article-actions">
-        <button class="btn btn-danger" data.id=${article._id}>Supprimer</button>
+        <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
         </div>`;
     return articleNode;
   });
   articleContainer.innerHTML = "";
   articleContainer.append(...articlesDOM);
+  const deleteBtn = articleContainer.querySelectorAll(".btn-danger");
+  deleteBtn.forEach(button => {
+    button.addEventListener("click", async event => {
+      event.preventDefault();
+      try {
+        const target = event.target;
+        const articleId = target.dataset.id;
+        const response = await fetch(`https://restapi.fr/api/dwwm_benjamin/${articleId}`, {
+          method: "DELETE"
+        });
+        const body = await response.json();
+        fetchArticles();
+        console.log(body);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
 };
 const fetchArticles = async () => {
   try {
     const response = await fetch("https://restapi.fr/api/dwwm_benjamin");
     const articles = await response.json();
-    if (!articles.length) {
-      displayArticles([articles]);
+    if (articles) {
+      if (!articles.length !== 0) {
+        displayArticles([articles]);
+      } else {
+        displayArticles(articles);
+      }
     } else {
-      displayArticles(articles);
+      articleContainer.innerHTML = "<p>Pas d'articles...<p>";
     }
   } catch (error) {
     console.log(error);
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const displayArticles = (articles) => {
         <p class="article-author">${article.author}</p>
         <p class="article-content">${article.content}</p>
         <div class="article-actions">
-        <button class="btn btn-danger" data.id=${article._id}>Supprimer</button>
+        <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
         </div>`;
 
     return articleNode;
@@ -25,6 +25,28 @@ const displayArticles = (articles) => {
 
   articleContainer.innerHTML = "";
   articleContainer.append(...articlesDOM);
+
+  const deleteBtn = articleContainer.querySelectorAll(".btn-danger");
+
+  deleteBtn.forEach((button) => {
+    button.addEventListener("click", async (event) => {
+      event.preventDefault();
+      try {
+        const target = event.target;
+        const articleId = target.dataset.id;
+
+        const response = await fetch(
+          `https://restapi.fr/api/dwwm_benjamin/${articleId}`,
+          { method: "DELETE" }
+        );
+        const body = await response.json();
+        fetchArticles();
+        console.log(body);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
 };
 
 const fetchArticles = async () => {
@@ -32,10 +54,14 @@ const fetchArticles = async () => {
     const response = await fetch("https://restapi.fr/api/dwwm_benjamin");
     const articles = await response.json();
 
-    if (!articles.length) {
-      displayArticles([articles]);
+    if (articles) {
+      if (!articles.length !== 0) {
+        displayArticles([articles]);
+      } else {
+        displayArticles(articles);
+      }
     } else {
-      displayArticles(articles);
+      articleContainer.innerHTML = "<p>Pas d'articles...<p>";
     }
   } catch (error) {
     console.log(error);
